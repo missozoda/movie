9644a6525366b1ab510bf840de5c75250ae6b068
missozoda <ozoshka1207@gmail.com>
2022-05-14 11:49:01 +0500

    add changes style

--- js/script.js
+++ js/script.js
@@ -1,7 +1,8 @@
 let elMovieList = $(".movie-list");
+let movie = kinolar.splice(1, 100)
 
-kinolar.forEach(function(kino){
-  let newLi = createElement("li", "col-3 border-2 border-dark my-3 p-2");
+movie.forEach(function(kino){
+  let newLi = createElement("li", "col-3 border border-2 border-success my-3 p-2");
   let newpTitle = createElement("p","title", `Title: ${kino.title}`);
   let newPYear = createElement("p", "year", `Year: ${kino.year}`);
   let newPCast = createElement("p", "cast", `Cast: ${kino.cast.join(", ")}`);
