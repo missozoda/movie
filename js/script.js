ad21aff41785b4edff988411c31ab8907e4f6810
missozoda <ozoshka1207@gmail.com>
2022-05-14 10:55:04 +0500

    add changes to js file

--- js/script.js
+++ js/script.js
@@ -1,11 +1,11 @@
 let elMovieList = $(".movie-list");
 
 kinolar.forEach(function(kino){
-  let newLi = createElement("li", "col-4 border-2 border-success my-3 p-2");
+  let newLi = createElement("li", "col-3 border-2 border-dark my-3 p-2");
   let newpTitle = createElement("p","title", kino.title);
   let newPYear = createElement("p", "year", kino.year);
-  let newPCast = createElement("p", "cast", kino.cast);
-  let newPGenres = createElement("p", "genres", kino.genres);
+  let newPCast = createElement("p", "cast", kino.cast.join(", "));
+  let newPGenres = createElement("p", "genres", kino.genres.join(", "));
 
   newLi.append(newpTitle, newPYear, newPCast, newPGenres);
   elMovieList.append(newLi);
