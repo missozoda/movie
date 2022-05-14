037a9457d84fe1768d8883edf6fa7d1cff0d3a31
missozoda <ozoshka1207@gmail.com>
2022-05-14 10:57:52 +0500

    add some changes

--- js/script.js
+++ js/script.js
@@ -2,10 +2,10 @@ let elMovieList = $(".movie-list");
 
 kinolar.forEach(function(kino){
   let newLi = createElement("li", "col-3 border-2 border-dark my-3 p-2");
-  let newpTitle = createElement("p","title", kino.title);
-  let newPYear = createElement("p", "year", kino.year);
-  let newPCast = createElement("p", "cast", kino.cast.join(", "));
-  let newPGenres = createElement("p", "genres", kino.genres.join(", "));
+  let newpTitle = createElement("p","title", `Title: ${kino.title}`);
+  let newPYear = createElement("p", "year", `Year: ${kino.year}`);
+  let newPCast = createElement("p", "cast", `Cast: ${kino.cast.join(", ")}`);
+  let newPGenres = createElement("p", "genres", `Genres: ${kino.genres.join(", ")}`add);
 
   newLi.append(newpTitle, newPYear, newPCast, newPGenres);
   elMovieList.append(newLi);
